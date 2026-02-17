d67427a5b87d1c32ade09dbac8d538a8753e38ad
Melqui Santos <melqsantos96@gmail.com>
2026-02-17 13:07:40 -0300

    fix: ajuste cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDatabase } from './config/database';
 import { routes } from './routes';
 dotenv.config();
 
 const app: Application = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
